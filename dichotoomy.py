06a8a2ac7e11242c4d4c686094ee1195fe18bfd3
ZDYC <332976499@qq.com>
2019-04-20 21:21:57 +0800

    full dichotomy

--- dichotoomy.py
+++ dichotoomy.py
@@ -1,10 +1,20 @@
 def dichotomy(lists,item):
+    """
+    dichotomy finds the index of the target number or string
+    """
     low = 0
     high = len(lists)
     while low <= high:
-        guess = high // 2
-        if lists[guess] == item:
-            return guess
+        mid = (low + high) // 2
+        guess = lists[mid]
+        if guess == item:
+            return mid
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
 
 if __name__ == '__main__':
-    dichotomy([1,2,3,4],3)
+    dichotomy([1,2,3,4],4)
+    dichotomy("python",'o')
