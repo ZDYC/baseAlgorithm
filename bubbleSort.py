a038a37e8a213b1440e47b2071075901eea22c70
ZDYC <332976499@qq.com>
2019-04-21 15:29:42 +0800

    bubblesort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,19 +1,24 @@
-def bubble(nlist,direction=True):
+def bubble(collections,direction=True):
     """
     bubble sort
     when direction is True,min to max,
     is False, max to min
+    Examples:
+    >>> bubble([0, 5, 3, 2, 2])
+    [0, 2, 2, 3, 5]
+    >>> bubble([0, 5, 3, 2, 2], direction=False)
+    [5, 3, 2, 2, 0]
     """
-    for i in range(0,len(nlist)-1):
-        for j in range(0,len(nlist)-i-1):
+    for i in range(0,len(collections)-1):
+        for j in range(0,len(collections)-i-1):
             if direction:
-                if(nlist[j] > nlist[j+1]):
-                    nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
+                if(collections[j] > collections[j+1]):
+                    collections[j], collections[j+1] = collections[j+1], collections[j]
             else:
-                if(nlist[j] < nlist[j+1]):
-                    nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
-    return nlist
+                if(collections[j] < collections[j+1]):
+                    collections[j], collections[j+1] = collections[j+1], collections[j]
+    return collections
 
 
 if __name__ == '__main__':
-    print(bubble([2,3,45,1]))
+    print(bubble([2,3,45,1], direction=False))
