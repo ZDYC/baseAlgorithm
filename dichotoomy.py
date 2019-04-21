a3c2963745fd045d96fe9a290b7bc68b9cc8106a
ZDYC <332976499@qq.com>
2019-04-21 22:31:11 +0800

    dic

--- dichotoomy.py
+++ dichotoomy.py
@@ -1,10 +1,14 @@
-def dichotomy(lists,item):
+def dichotomy(collections,item):
     low = 0
-    high = len(lists)
+    high = len(collections)
     while low <= high:
-        guess = high // 2
-        if lists[guess] == item:
-            return guess
+        mid = (high + low) // 2
+        if collections[mid] == item:
+            return mid
+        elif collections[mid] > item:
+            pass
+        else:
+            pass
 
 if __name__ == '__main__':
     dichotomy([1,2,3,4],3)
